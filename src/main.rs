0836af099605edf63ddb14fd852c58ea459d57f1
Karl San Gabriel <karl.sangabriel@gmail.com>
2022-02-18 08:54:56 +0800

    Sort output table by sort key that may be available in the destination file name. If not speficied, default sort key is by Id.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #[macro_use] extern crate prettytable;
+
+use std::cmp::Ordering;
 use std::env;
 use serde::Serialize;
 use serde::Deserialize;
 use prettytable::{Table, Row, Cell};
 use std::fs;
+use std::path::Path;
 
-// Pastry struct
+static SORT_COMMAND_KEYWORD: &str = "sortby";
+static DEFAULT_SORT_KEY: &str = "Id";
+static AVAILABLE_SORT_KEYS:[&str; 5] = ["Id", "Type", "Name", "Batter", "Topping"];
+
+/// Pastry struct
 #[derive(Serialize, Deserialize, Debug)]
 struct Pastry {
     Id: String,
@@ -15,14 +22,49 @@ struct Pastry {
     Topping: String
 }
 
-// TODO: Exception handling and unit tests
+/// Retrieve sort key if sort command is used in the destination file
+fn get_sort_key_from_filename(dest_filename: &String) -> &str {
+    let path:&Path = Path::new(dest_filename);
+    let filename:&str = path.file_name().unwrap().to_str().unwrap();
+    let mut sort_key = DEFAULT_SORT_KEY;
+
+    // Loop through available sort keys and return match
+    if filename.contains(SORT_COMMAND_KEYWORD) {
+        for available_for_key in AVAILABLE_SORT_KEYS {
+            if filename.to_lowercase().contains(&available_for_key.to_lowercase()) {
+                return available_for_key;
+            }
+        }
+    }
+    return sort_key;
+}
+
+/// Sort pastries by sort key
+fn sort_pastries(pastries: &mut Vec<Pastry>, sort_key: &str) {
+
+    let sort_closure_operation:fn(first: &Pastry, second: &Pastry)-> Ordering = match sort_key {
+        "Id" => |a, b| a.Id.cmp(&b.Id),
+        "Type" => |a, b| a.Type.cmp(&b.Type),
+        "Name" => |a, b| a.Name.cmp(&b.Name),
+        "Batter" => |a, b| a.Batter.cmp(&b.Batter),
+        "Topping" => |a, b| a.Topping.cmp(&b.Topping),
+        _ => |a, b| a.Id.cmp(&b.Id),
+    };
+
+    // Sort pastries by chosen closure operation
+    pastries.sort_by(sort_closure_operation);
+}
+
+/// TODO: Exception handling and unit tests
 fn main() {
 
     // Usage: [path to your program] [input file path] [output file path]
     let args: Vec<String> = env::args().collect();
 
-    let source_json_file = &args[1];
-    let dest_table_file = &args[2];
+    let source_json_file:&String = &args[1];
+    let dest_table_file:&String = &args[2];
+
+    let sort_key = get_sort_key_from_filename(dest_table_file);
 
     // Read the content of JSON file into a string
     let json_string = fs::read_to_string(source_json_file).expect("Unable to read JSON file");
@@ -30,8 +72,7 @@ fn main() {
     // Deserialize JSON into a collection of pastries
     let mut pastries: Vec<Pastry> = serde_json::from_str(&json_string).unwrap();
 
-    // Sort pastries by Id in ascending order
-    pastries.sort_by(|a, b| a.Id.cmp(&b.Id));
+    sort_pastries(&mut pastries, &sort_key);
 
     let mut table = Table::new();
 
